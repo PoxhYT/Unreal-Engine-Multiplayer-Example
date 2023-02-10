74630d4b49d3c947b414b08181737573f88e6a19
Michael Ernst <72230632+PoxhYT@users.noreply.github.com>
2023-02-10 23:11:41 +0100

    Added project

---
+++ MultiplayerExample/Source/MultiplayerExample/MultiplayerExample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiplayerExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiplayerExample, "MultiplayerExample" );
